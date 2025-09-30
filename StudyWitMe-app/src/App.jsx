0ba3ea0952bb02404d4d354a9fd3edd1e8b3c81d
Dm4rt <danmartinez2004@gmail.com>
2025-09-29 23:13:20 -0400

    working on test signup page

--- StudyWitMe-app/src/App.jsx
+++ StudyWitMe-app/src/App.jsx
@@ -19,10 +19,23 @@ function App() {
     }))
   }
 
+  const handleSubmit=(e) =>{
+    e.preventDefault();
+    console.log("Signing up: ", formData)
+    //we'll put the firebase complicated stuff ehre
+  }
+
   return (
-    <>
-      <h1>test</h1>
-    </>
+      <nav className="navbar">
+        <div className="nav-left">
+          <img src={logo} alt="robot logo" className="nav-logo"/>  
+        </div>
+        <div className="nav-right">
+          <a href="">Signup</a>
+          <a href="">Login</a>
+          <a href="">Flashcards</a>
+        </div>
+      </nav>
   )
 }
 
