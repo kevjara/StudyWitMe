935b73057a14e15457f4251b87d9447f032e87c8
Elvis Dominguez <elvis.dominguez45@myhunter.cuny.edu>
2025-10-04 20:54:09 -0400

    Integrated Firebase into the website. Added gloabl auth context for tracking current user state. Updated Login, MainMenu, and Flashcards View. Updated Background Animation.

--- StudyWitMe-app/src/App.jsx
+++ StudyWitMe-app/src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import TitleScreen from "./components/TitleScreen";
 import MainMenu from "./components/MainMenu";
 import Login from "./components/Login";
 import FlashcardGenerator from "./components/FlashcardGenerator";
 import Background from "./components/Background";
+import Flashcards from "./components/Flashcards";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <Router>
-      <>
+    <>
       <Background />
       <Routes>
         <Route path="/" element={<TitleScreen />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<MainMenu />} />
         <Route path="/flashcards/create" element={<FlashcardGenerator />} />
+        <Route path="/flashcards/view" element={<Flashcards />} />
+        <Route path="profile" element={<Profile />} />
       </Routes>
-      </>
-    </Router>
+    </>
   );
 }
 
